5915f5663bb0035e2661a52e1115ce60886bf459
Francisco Garciarena <francisco.garciarena94@gmail.com>
2022-11-04 13:32:59 -0300

    conditional navbar

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,6 +1,6 @@
 // Libraries
 import { Link, useNavigate } from 'react-router-dom'
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 
 // Redux
 import { logout, reset } from '../../features/auth/authSlice'
@@ -12,6 +12,8 @@ const Header = () => {
   const navigate = useNavigate()
   const dispatch = useDispatch()
 
+  const { user } = useSelector((state) => state.auth)
+
   const onLogout = () => {
     dispatch(logout())
     dispatch(reset())
@@ -20,25 +22,35 @@ const Header = () => {
   return (
     <header className={styled.container}>
       <nav>
-        <ul>
-          <li>
-            <Link to='/'>home</Link>
-          </li>
-          <li>
-            <Link to='/login'>login</Link>
-          </li>
-          <li>
-            <Link to='/register'>register</Link>
-          </li>
-          <li>
-            <Link to='/login' onClick={onLogout}>
-              logout
-            </Link>
-          </li>
-          <li>
-            <Link to='/test'>test</Link>
-          </li>
-        </ul>
+        {!user ? (
+          <ul>
+            <li>
+              <Link to='/'>home</Link>
+            </li>
+            <li>
+              <Link to='/login'>login</Link>
+            </li>
+            <li>
+              <Link to='/register'>register</Link>
+            </li>
+          </ul>
+        ) : (
+          <ul>
+            <li>
+              <Link to='/'>home</Link>
+            </li>
+            <li>
+              <Link to='/login' onClick={onLogout}>
+                logout
+              </Link>
+            </li>
+            <li>
+              <Link to='/test'>
+                {user.first_name} {user.last_name}
+              </Link>
+            </li>
+          </ul>
+        )}
       </nav>
     </header>
   )
